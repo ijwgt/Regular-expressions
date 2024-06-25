e6feac4049f35041902bed622a9dd079af5e1e80
ijwgt <66661966@bk.ru>
2024-06-25 17:59:20 +0300

    test

--- js/regex.js
+++
@@ -1,5 +0,0 @@
-export default class Validator {
-	validatorUsername(username) {
-		return /^(?!\d{4,})[a-zA-Z][\da-zA-Z-_]*[a-zA-Z]$/
-	}
-}
--- js/test/test.js
+++
@@ -1,5 +0,0 @@
-import Validator from "../regex"
-
-test.each([
-	
-])
